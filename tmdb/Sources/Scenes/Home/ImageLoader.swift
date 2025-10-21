9c5057e8a680af7d6c2283bdf27ca728c0487770
Diogo <diogoteixeiralucas@pm.me>
2025-10-21 09:46:11 +0100

    Add ImageLoader singleton with caching

---
+++ tmdb/Sources/Scenes/Home/ImageLoader.swift
@@ -0,0 +1,52 @@
+//
+//  ImageLoader.swift
+//  tmdb
+//
+//  Created by Diogo on 19/10/2025.
+//
+
+import UIKit
+
+final class ImageLoader {
+    static let shared = ImageLoader()
+    
+    private let cache = NSCache<NSString, UIImage>()
+    private let session: URLSession
+    
+    private init(session: URLSession = .shared) {
+        self.session = session
+        // Opcional: configurar limites de cache
+        cache.countLimit = 500
+        cache.totalCostLimit = 50 * 1024 * 1024 // ~50MB
+    }
+    
+    func load(url: URL, completion: @escaping (UIImage?) -> Void) {
+        let key = url.absoluteString as NSString
+        
+        if let cached = cache.object(forKey: key) {
+            DispatchQueue.main.async {
+                completion(cached)
+            }
+            return
+        }
+        
+        session.dataTask(with: url) { [weak self] data, response, error in
+            guard
+                error == nil,
+                let data = data,
+                let image = UIImage(data: data)
+            else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            self?.cache.setObject(image, forKey: key, cost: data.count)
+            
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }.resume()
+    }
+}
